c8c4fc3688a7f2bdbb8ec253cfedaaaad5fd3f5e
ejnic <ejnicodemus@gmail.com>
2020-07-06 15:21:04 -0400

    updates

--- processprograms.py
+++ processprograms.py
@@ -7,15 +7,10 @@ import myfunctions as mf
 engine = sqlalchemy.create_engine(pr.strengine)
 
 df = pd.read_sql_query(pr.sqlprograms, engine)
-print(df.shape)
 # get just Bloomington programs
 dfbl = df[df.institution == 'IUBLA']
-print(dfbl.shape)
+
+#take the number off the end if it exists
 dfbl['cleanedprogramcd'] = dfbl['acad_prog']
 dfbl['cleanedprogramcd'] = dfbl['cleanedprogramcd'].apply(mf.cleanprogramcd)
 print(dfbl.head(10))
-#dfbl['cleanedprogramcd']=dfbl['cleanedprogramcd'].apply(cleanprogramcd(dfbl[['acad_prog']]))
-#dfbl['cleanedprogramcd'] = mf.cleanprogramcd(dfbl[['acad_prog']])
-#for index, row in dfbl.iterrows():
-    #programcd = ProgramCode(dfbl.iloc[index]['acad_prog']).cleancd
-#    print(classprocess.cleanprogramcd(dfbl.iloc[index]['acad_prog']))
